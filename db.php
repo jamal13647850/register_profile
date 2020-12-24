14b88799f9e408c7715ce75d49de50d440f58704
Sayyed Jamal Ghasemi <jamal13647850@gmail.com>
2020-12-24 16:43:30 +0330

    Initial

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php 
+$mysql_hostname = "mysql73";
+$mysql_user = "root";
+$mysql_password = "test";
+$mysql_database = "userprofile";
+$con = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database);
+?>
